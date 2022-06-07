4e5f210ff0f47cd0cf73dd731bb7a5ee2ae1879e
dodo <damiandominella@gmail.com>
2022-06-07 14:04:41 +0200

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,35 @@
-import { Code } from "@react-meetup/ui-kit.code";
-import { Container } from "@react-meetup/ui-kit.container";
-import { Hero } from "@react-meetup/ui-kit.hero";
-import { Toolbar } from "@react-meetup/ui-kit.toolbar";
-import { Text } from "@react-meetup/ui-kit.typography.text";
+import { Button } from "@gle/base-ui.buttons.button";
+import { EmojiFeedback } from "@gle/base-ui.emoji-feedback";
+import { Theme } from "@gle/base-ui.theme-provider";
+import { Chip } from "@gle/base-ui.typography.chip";
+import { Heading } from "@gle/base-ui.typography.heading";
 import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Toolbar title={"component-driven-development"} />
-      <Hero
-        title="I am the mobile app"
-        subtitle="I import components as dependencies"
-      />
+    <Theme>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          gap: 20,
+        }}
+      >
+        <Heading>I'm the consumer app</Heading>
+        <Heading level={3}>
+          I'm using components imported from{" "}
+          <Chip color="danger">@gle.base-ui</Chip>
+        </Heading>
 
-      <Container>
-        <Text>
-          You can import and edit components and publish them directly from this
-          repository, without worrying about <strong>versioning</strong>,{" "}
-          <strong>compiling</strong>, <strong>package.json</strong> etc...
-        </Text>
+        <Button>Click me</Button>
 
-        <div
-          style={{
-            display: "inline-flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            gap: 10,
-          }}
-        >
-          <Code>bit init</Code>
-
-          <Code>{"bit import <your-component-id>"}</Code>
-
-          <Code>bit tag</Code>
-
-          <Code>bit status</Code>
-
-          <Code>bit export --eject</Code>
-        </div>
-      </Container>
-    </div>
+        <br />
+        <EmojiFeedback title="How would you rate bit.dev?" />
+      </div>
+    </Theme>
   );
 }
 
