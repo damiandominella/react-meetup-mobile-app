53dba495f6790559d62ff609e8c49fa8a619f4cd
dodo <damiandominella@gmail.com>
2022-06-07 09:41:11 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Heading } from "@react-meetup/ui-kit.typography.heading";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading>I am the mobile app</Heading>
+      <Heading level={3}>I'm using components as dependencies</Heading>
     </div>
   );
 }
